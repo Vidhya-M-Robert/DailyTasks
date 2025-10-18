2d5b597bd375f565bcbf50881a58d46af2d3a5eb
Vidhya-M-Robert <vidhya.robert@vonnue.com>
2025-10-18 22:14:58 +0530

    day4 task5

--- Day-4/second-task5/script.js
+++ Day-4/second-task5/script.js
@@ -1,3 +1,7 @@
 const input = document.getElementById("input");
 const listItem = document.getElementById("listItems");
 
+input.addEventListener("input", ()=>{
+    let output = input.value
+    listItem.textContent = `Hello ${output}`;
+})
