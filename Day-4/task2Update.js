1fb12eefb821e5e23f5af6b9c304c61497d49456
Vidhya-M-Robert <vidhya.robert@vonnue.com>
2025-11-08 23:29:11 +0530

    permutation task

---
+++ Day-4/task2Update.js
@@ -0,0 +1,21 @@
+function permutations(str) {
+  if (str.length <= 1) return [str];
+
+  let result = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    let remaining = str.slice(0, i) + str.slice(i + 1);
+
+    let remainingPermutations = permutations(remaining);
+
+    remainingPermutations.forEach((perm) => {
+      result.push(char + perm);
+    });
+  }
+
+  return result;
+}
+
+console.log(permutations("abc"));
